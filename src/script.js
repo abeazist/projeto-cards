bdf39813e98436037993456f3b487b06c69b4c25
beatriz <128046453+abeazist@users.noreply.github.com>
2024-06-28 09:57:02 -0300

    começar o repositorios

--- src/script.js
+++ src/script.js
@@ -5,12 +5,12 @@ botaoCartao.addEventListener('click', () => {
     const username = document.getElementById('usuario').value;
     if (username) {
         fetch(`https://api.github.com/users/${username}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.message === 'Not Found') {
+            .then(resposta => resposta.json()) //o response é uma funcao que vai devolver a resposta como objeto 
+            .then(dados => {
+                if (dados.message === 'Not Found') {
                     alert('Usuário não encontrado!');
                 } else {
-                    adicionarCartao(data);
+                    adicionarCartao(dados);
                 }
             })
             .catch(error => {
@@ -23,18 +23,15 @@ botaoCartao.addEventListener('click', () => {
 
 function adicionarCartao(user) {
     const card = document.createElement('div');
-    card.className = 'flex flex-col items-center w-1/5 h-auto border rounded-md m-4 p-4 bg-white shadow-lg';
-    card.innerHTML = `<div id="cartoes" class="flex flex-col items-center flex-wrap w-1/5 h-96 border rounded-md">
-        <img class="h-40" src="github-pages-1024x512.jpg">
-        <div class="w-24 h-24 rounded-full overflow-hidden border-4 border-white">
-                        <img class="w-full h-full object-cover" src="${user.avatar_url}" alt="Profile Picture">
-                        
-        </div>
-    </div>
-                      <p class="mt-4 font-bold">${user.name}</p>
+    card.className = 'flex';
+    card.innerHTML =`<div id="cartoes" class="flex flex-col items-center w-64 h-96 border rounded-md">
+        <img class="h-36" src="github-pages-1024x512.jpg">
+        <img class="w-20 h-20 rounded-full object-cover absolute top-40 border-4 border-white" src="${user.avatar_url}" alt="Profile Picture">         <br>
+        <p class="mt-4 font-bold">${user.name}</p>
                       <p class="text-gray-500">@${user.login}</p>
                       <strong class="mt-2">REPOSITÓRIOS</strong>
-                      <p>${user.public_repos}</p>`;
-
+                      <p>${user.repos_url}</p>
+                      
+</div>` 
     cartoes.appendChild(card);
 }
