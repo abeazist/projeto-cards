7f79c8d74dbd06c5b2e848284b7a85affd13cc07
beatriz <128046453+abeazist@users.noreply.github.com>
2024-06-28 23:56:54 -0300

    tentando por o repositório

--- src/script.js
+++ src/script.js
@@ -5,20 +5,25 @@ botaoCartao.addEventListener('click', () => {
     const username = document.getElementById('usuario').value;
     if (username) {
         fetch(`https://api.github.com/users/${username}`)
-            .then(resposta => resposta.json()) //o response é uma funcao que vai devolver a resposta como objeto 
-            .then(dados => {
-                if (dados.message === 'Not Found') {
-                    alert('Usuário não encontrado!');
-                } else {
-                    adicionarCartao(dados);
-                }
-            })
-            .catch(error => {
-                console.error('Erro ao buscar dados:', error);
-            });
-    } else {
-        alert('Por favor, insira um nome de usuário!');
-    }
+        .then(resposta => resposta.json()) //o response é uma funcao que vai devolver a resposta como objeto 
+        .then(dados => {
+            if (dados.message === 'Not Found') {
+                alert('Usuário não encontrado!');
+            } else {
+                adicionarCartao(dados);
+            }
+            const repos = document.getElementById('repos').value;
+        })}if (repos) {
+                fetch(`https://api.github.com/repos/${repos}`)
+                .then(resposta => resposta.json())
+                
+            }
+    //         .catch(error => {
+    //             console.error('Erro ao buscar dados:', error);
+    //         });
+    // } else {
+    //     alert('Por favor, insira um nome de usuário!');
+    // }
     
 });
 
@@ -30,8 +35,12 @@ function adicionarCartao(user) {
         <img class="w-20 h-20 rounded-full object-cover absolute top-40 border-4 border-white" src="${user.avatar_url}" alt="Profile Picture">         <br>
         <p class="mt-4 font-bold">${user.name}</p>
                       <p class="text-gray-500">@${user.login}</p>
-                      <strong class="mt-2">REPOSITÓRIOS</strong>
-                      <p>${user.repos_url}</p>
+                      <div class="flex justify-start rounded-sm"><strong class="mt-2">REPOSITÓRIOS</strong></div>
+                      <div class="flex w-56 h-20 bg-gray-200 justify-center rounded-sm items-center">
+                        <p id='repositorio'class="text-black">${repos.name}</p>
+                        <p id='repositorio' class="text-gray-800">${repos.description}</p>
+                        <div class='bg-slate-400 text-black rounded-sm'><p id='repositorio'>${repos.language}</p></div>
+                    </div>
                       
 </div>` 
     cartoes.appendChild(card);
